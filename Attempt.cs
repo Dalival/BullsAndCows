83522a6edb69f34bb4aa47894bf35e43ab241067
Dalival <egorfedorenko.w@gmail.com>
2021-11-19 17:47:42 +0300

    Add Attempt class

---
+++ Attempt.cs
@@ -0,0 +1,50 @@
+﻿using System.Text;
+
+namespace BullsAndCows
+{
+    public class Attempt
+    {
+        public Number Number { get; set; }
+
+        public int Bulls { get; set; }
+
+        public int Cows { get; set; }
+
+
+        public Attempt(Number number, int bulls, int cows)
+        {
+            Number = number;
+            Bulls = bulls;
+            Cows = cows;
+        }
+
+
+        public override string ToString()
+        {
+            if (Cows == 4)
+            {
+                return "WIN!";
+            }
+
+            var str = new StringBuilder();
+            str.Append($"{Number} – ");
+
+            if (Bulls != 0)
+            {
+                str.Append(Bulls + "B ");
+            }
+
+            if (Cows != 0)
+            {
+                str.Append(Cows + "C");
+            }
+
+            if (Bulls == 0 && Cows == 0)
+            {
+                str.Append("nothing");
+            }
+
+            return str.ToString();
+        }
+    }
+}
