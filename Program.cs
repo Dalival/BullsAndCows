3e0f21e61ab12aa1ef5242a13f7178506f9d1809
Dalival <egorfedorenko.w@gmail.com>
2021-12-10 17:51:16 +0300

    Update opening text

--- Program.cs
+++ Program.cs
@@ -6,11 +6,16 @@ namespace BullsAndCows
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("\nThis is a test sample of the \"Bulls and Cows\" game." +
-                              "\nPick a number and then try to guess the computer's number." +
-                              "\nFor now computer will not try to guess your number." +
-                              "\nIt skips it's turn every time. On this step it's important" +
-                              "\nto teach it to answer your turns.\n");
+            Console.WriteLine("On a sheet of paper, the players each write a 4-digit secret number." +
+                              "\nThe digits must be all different. Then, in turn, the players try to" +
+                              "\nguess their opponent's number who gives the number of matches. If the" +
+                              "\nmatching digits are in their right positions, they are cows, if in" +
+                              "\ndifferent positions, they are bulls. Example:" +
+                              "\n\nSecret number: 4271" +
+                              "\nOpponent's try: 1234" +
+                              "\nAnswer: 1C 2B (1 cow and 2 bulls)" +
+                              "\n\nPress any key to play the game");
+            Console.ReadKey();
             var game = new Game();
             game.Start();
 
