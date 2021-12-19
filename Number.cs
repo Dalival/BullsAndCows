fbfa06bd005b8fa76ed3bf1a9622a92c44217a18
Dalival <egorfedorenko.w@gmail.com>
2021-12-19 20:24:44 +0300

    Codestyle fix

--- Number.cs
+++ Number.cs
@@ -71,25 +71,16 @@ namespace BullsAndCows
 
         public override string ToString() => $"{Digits[0]}{Digits[1]}{Digits[2]}{Digits[3]}";
 
-        public override bool Equals(object? o)
-        {
-            return o is Number other && Equals(other);
-        }
+        public override bool Equals(object o) => o is Number other && Equals(other);
 
-        public override int GetHashCode()
-        {
-            return Digits != null
-                ? HashCode.Combine(Digits[0], Digits[1], Digits[2], Digits[3])
-                : 0;
-        }
+        public override int GetHashCode() => Digits != null
+            ? HashCode.Combine(Digits[0], Digits[1], Digits[2], Digits[3])
+            : 0;
 
 
-        private bool Equals(Number other)
-        {
-            return Digits[0] == other.Digits[0]
-                && Digits[1] == other.Digits[1]
-                && Digits[2] == other.Digits[2]
-                && Digits[3] == other.Digits[3];
-        }
+        private bool Equals(Number other) => Digits[0] == other.Digits[0]
+                                             && Digits[1] == other.Digits[1]
+                                             && Digits[2] == other.Digits[2]
+                                             && Digits[3] == other.Digits[3];
     }
 }
