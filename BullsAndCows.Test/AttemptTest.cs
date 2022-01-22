4a439da0df3c1991d27388c987ff4599ae7b4cf2
Dalival <egorfedorenko.w@gmail.com>
2022-01-22 19:33:31 +0300

    Add unit tests

---
+++ BullsAndCows.Test/AttemptTest.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+
+namespace BullsAndCows.Test
+{
+    [TestFixture]
+    public class AttemptTest
+    {
+        [Test]
+        public void Constructor_NumberIsNull_Exception()
+        {
+            Assert.Catch<ArgumentNullException>(() => new Attempt(null, 2, 3));
+        }
+
+        [Test]
+        [TestCase(5, 0)]
+        [TestCase(0, 5)]
+        [TestCase(-1, 2)]
+        [TestCase(3, -2)]
+        [TestCase(-1, -2)]
+        [TestCase(1, 4)]
+        [TestCase(2, 3)]
+        [TestCase(1, 3)]
+        public void Constructor_WrongBullsAndCowsAmounts_Exception(int bulls, int cows)
+        {
+            var validNumber = new Number(1234);
+            Assert.Catch<Exception>(() => new Attempt(validNumber, bulls, cows));
+        }
+
+        [Test]
+        public void Equals_SameAttempts_True()
+        {
+            var numberOne = new Number(1234);
+            var numberTwo = new Number(1234);
+
+            var attemptOne = new Attempt(numberOne, 1, 2);
+            var attemptTwo = new Attempt(numberTwo, 1, 2);
+
+            Assert.AreEqual(attemptOne, attemptTwo);
+        }
+    }
+}
---
+++ BullsAndCows.Test/NumberTest.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+
+namespace BullsAndCows.Test
+{
+    [TestFixture]
+    public class NumberTest
+    {
+        [Test]
+        [TestCase(54)]
+        [TestCase(1841)]
+        [TestCase(12345)]
+        public void IsIntAllowed_WrongNumber_False(int numberInt)
+        {
+            var result = Number.IsIntAllowed(numberInt);
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        [TestCase(123)]
+        [TestCase(9876)]
+        [TestCase(0821)]
+        [TestCase(5678)]
+        public void IsIntAllowed_ValidNumber_True(int numberInt)
+        {
+            var result = Number.IsIntAllowed(numberInt);
+            Assert.IsTrue(result);
+        }
+    }
+}
